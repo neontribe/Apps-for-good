4b02a913299991cef852dbed811172e29722642a
Toby Batch <tobias@neontribe.co.uk>
2011-06-28 14:38:06 +0100

    Add toLowerCase to the purl creation

--- sites/all/modules/afg_groups_views/afg_groups_views.js
+++ sites/all/modules/afg_groups_views/afg_groups_views.js
@@ -1,7 +1,7 @@
 Drupal.behaviors.ntforms = function (context) { 
   $('#edit-title').change(function() {
     var rawname = $('#edit-title').val();
-    var newname = rawname.replace(/[^a-zA-Z0-9]/g, '-');
+    var newname = rawname.replace(/[^a-zA-Z0-9]/g, '-').toLowerCase();
     console.log(newname);
     $('#edit-purl-value').val(newname);
   });
