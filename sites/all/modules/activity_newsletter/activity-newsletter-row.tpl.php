6d10c60045f7dbb6d97d62b09d6210c099c534a9
Katja Mordaunt <katja@neontribe.co.uk>
2011-07-07 17:25:01 +0100

    added row template for newsletter

---
+++ sites/all/modules/activity_newsletter/activity-newsletter-row.tpl.php
@@ -0,0 +1,10 @@
+<?php
+	$time = date('c', $timestamp);
+	$link = url("node/$node->nid", array('absolute' => TRUE));
+	echo "$title at $link was $operation at $time by $by";
+	if(!empty($node->teaser)) {
+		echo ":\n\n\t$node->teaser";
+	} else {
+		echo ".";
+	}
+?>
