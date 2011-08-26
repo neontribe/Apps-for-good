506aaa576e19b0d81770a955fbb7c73651ee6ba0
Matt Layton <Matt Layton@.(none)>
2011-08-26 11:54:12 +0100

    Changed navigation link to 'centres'. Added style.

--- sites/all/themes/rubik/afg/afg.js
+++ sites/all/themes/rubik/afg/afg.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
 	$('#content-left .block').append('<div class="block-shadow"></div>');
 	$('#content-left #stats .item').append('<span class="item-arrow"></span>');
+	
+	// Toggle 'My Account', 'My Groups' menu
+	$('#block-atrium-account .pane-left h3').click(function(){
+		$('#block-atrium-account .pane-right h3, #block-atrium-account .pane-left ul, #block-atrium-account .pane-right ul').toggle();
+	});
 });
 
 //$(window).load(function() {
