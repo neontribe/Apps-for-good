0ce7b2d644c05f908772cf8c73b87b64d8673d63
Toby Batch <tobias@neontribe.co.uk>
2011-08-26 12:20:14 +0100

    Moved function from template.php(s) to a module this allows theme switching to work

--- sites/all/themes/appsforgood/template.php
+++ sites/all/themes/appsforgood/template.php
@@ -35,18 +35,7 @@ function appsforgood_preprocess_box(&$vars, $hook) {
 }
 
 /*count funtions*/
-
-function afg_user_count() {
-$count_users = db_result(db_query("SELECT COUNT(uid) FROM {users}"));
-return $count_users;
-}
-
-
-function afg_content_count($type) {
-$count_content = db_result(db_query("SELECT COUNT(*) FROM {node} WHERE type = '%s' AND status = 1", $type));
-return $count_content;
-}
-
+// Moved to module afg_theme_updates to allow theme switching to work
 
 /**Shoutbox comment form**/
 function afg_shoutbox_comment_form() {
--- sites/all/themes/rubik/afg/template.php
+++ sites/all/themes/rubik/afg/template.php
@@ -119,27 +119,10 @@ function afg_preprocess_box(&$vars, $hook) {
 }
 
 /*count funtions*/
-
-function afg_user_count() {
-$count_users = db_result(db_query("SELECT COUNT(uid) FROM {users}"));
-return $count_users;
-}
-
-
-function afg_content_count($type) {
-$count_content = db_result(db_query("SELECT COUNT(*) FROM {node} WHERE type = '%s' AND status = 1", $type));
-return $count_content;
-}
-
+// Moved to module afg_theme_updates to allow theme switching to work
 
 /**Shoutbox comment form**/
-
-function afg_shoutbox_comment_form() {
-  $shoutbox = atrium_shoutbox_get_shoutbox();
-  $view = views_get_view('shoutbox_shouts');
-  $rendered = $view->execute_display('comment_block');
-  return drupal_get_form('atrium_shoutbox_shoutform', $shoutbox['node'], $view, $rendered);
-}
+// Moved to module afg_theme_updates to allow theme switching to work
 
 /* User links block */
 
