0163190b59986be539124fbba01e13d34d400839
katjam <katjamordaunt@gmail.com>
2011-09-08 15:53:19 +0100

    Final push of untracked files

---
+++ sites/default/drushrc.php
@@ -0,0 +1,27 @@
+<?php
+
+$options['skip-tables'] = array(
+  'common' => array(
+    'cache',
+    'cache_block',
+    'cache_content',
+    'cache_emfield_xml',
+    'cache_filter',
+    'cache_form',
+    'cache_location',
+    'cache_media_youtube_status',
+    'cache_menu',
+    'cache_page',
+    'cache_rules',
+    'cache_tax_image',
+    'cache_update',
+    'cache_views',
+    'cache_views_data',
+    'history',
+    'sessions',
+    'watchdog'
+  ),
+ );
+ 
+// vim: filetype=php:
+
