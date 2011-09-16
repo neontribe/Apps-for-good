b9321e71ac1ba9527c5a85999927e7af2537d4a0
Matt Layton <matt@neontribe.co.uk>
2011-09-16 10:01:15 +0100

    Social stuff fix

--- sites/all/themes/rubik/afg/page.tpl.php
+++ sites/all/themes/rubik/afg/page.tpl.php
@@ -8,6 +8,8 @@
    
   <div id='content'>
   
+  <?php print $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>
+  
     <?php if ($tabs): ?>
       <div id='tabs'>
         <div class='page-tabs limiter clear-block'><?php print $tabs ?></div>
